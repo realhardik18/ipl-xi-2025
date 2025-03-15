122f023a632f4a6560cbc19c40d1dfa15d5f49d3
hardik singh <realhardik18@gmail.com>
2025-03-15 19:15:34 +0530

    working on backend routes

---
+++ app/page.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Home() {
+  const Teams=[]
+  for(let i=0;i<10;i++){
+    Teams.push("hardik")
+  }
+  console.log(Teams)
+  return (
+    <div className="m-5">
+      <div>
+        <p className="text-4xl text-white">IPL Squads 2025</p>
+      </div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="grid grid-cols-2 gap-4">        
+          {Teams.map((item,index)=>(
+            <p key={index} className="text-red-400">{item}</p>
+          ))}              
+        </div>
+      </div>
+
+
+    </div>
+  );
+}
